71b8dfc1022420917dc5aa9175ae4cf3ee51e40d
EushaMashfi <31277813+EushaMashfi@users.noreply.github.com>
2021-12-01 12:51:26 +0600

    'sixth'

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res) => {
-    res.send('Welcome Azure people')
+    res.send('Welcome Azure lab b')
 });
 
 app.listen(8080, ()=> {
-    console.log('Server is running on 3000')
+    console.log('Server is running on 8080')
 });
